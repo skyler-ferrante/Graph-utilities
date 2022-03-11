b1fe8e58f473408986e7e7f739434072dc89cbd4
skyler-ferrante <sjf5462@rit.edu>
2022-03-11 13:22:10 -0500

    Adding tests for pie chart

--- graph_test.sh
+++ graph_test.sh
@@ -3,9 +3,11 @@
 # Test with titles
 ./make_box.py "Make box plot" <data/box.txt &
 ./make_scatter.py "Make scatter plot" <data/scatter.txt &
-./make_bar.py "Make bar chart" <data/bar.txt &
+./make_bar.py "Make bar chart" <data/bar_and_pie.txt &
+./make_pie.py "Make pie chart" <data/bar_and_pie.txt &
 
 # Test without titles
 ./make_box.py <data/box.txt &
 ./make_scatter.py <data/scatter.txt &
-./make_bar.py <data/bar.txt &
+./make_bar.py <data/bar_and_pie.txt &
+./make_pie.py <data/bar_and_pie.txt &
